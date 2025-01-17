805c696472c401134657258404cb2e054ccdf93b
Mircea Maierean <mircea.maierean@yahoo.com>
2025-01-17 11:01:06 +0200

    change language to romanian

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -1,10 +1,11 @@
 import { Message } from "@/lib/types";
 
 export function ChatMessage({ message }: { message: Message }) {
+  if (message.question === "") return;
   return (
     <>
       <div className="flex justify-end">
-        <div className="max-w-3/4 p-3 rounded-lg bg-[#498C8A]">
+        <div className="max-w-3/4 p-3 rounded-lg bg-[#498C8A] text-white">
           <p>{message.question}</p>
         </div>
       </div>
